cd13daba475940586c8d7187bad180eac0df26da
Vladislav Vasilev <vyruscd@gmail.com>
2026-06-13 19:46:39 +0400

    refactor(repository/secret): prevent read file with a key every time

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -17,6 +17,8 @@ JWT_ALGORITHM = "HS256"
 ACCESS_TOKEN_TTL = 60*60*12  # 12 hours
 REFRESH_TOKEN_TTL = 60*60*24*7  # 7 days
 
+JWT_KEY_FILE_PATH = BASE_DIR / "key"
+
 
 class AppSettings(BaseSettings):
     debug: bool
@@ -35,3 +37,30 @@ class DatabaseSettings(BaseSettings):
         env_file=BASE_DIR / ".env",
         extra="ignore",
     )
+
+
+class CORSSettings(BaseSettings):
+    origins: list[str]
+    allow_methods: list[str]
+    allow_headers: list[str]
+    allow_credentials: bool
+
+    model_config = SettingsConfigDict(
+        yaml_file="config.yaml",
+        yaml_config_section="cors",
+        yaml_file_encoding="utf8",
+        extra="ignore",
+        case_sensitive=False,
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (YamlConfigSettingsSource(settings_cls),)
+
