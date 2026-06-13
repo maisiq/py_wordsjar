c590e8b3d1e7925506c62d281392b614ad8fbf2b
Vladislav Vasilev <vyruscd@gmail.com>
2026-06-13 19:37:37 +0400

    refactor(repository/token): use config var for token store duration

--- backend/app/repository/token.py
+++ backend/app/repository/token.py
@@ -1,8 +1,9 @@
 from typing import Any
 
 from cachetools import TTLCache
+from core import config
 
-_data = TTLCache(256, 60*60*12)
+_data = TTLCache(256, config.REFRESH_TOKEN_TTL)
 
 
 class TokenStorage:
